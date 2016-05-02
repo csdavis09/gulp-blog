1d75d79b7bd970a1a0d02c1eeb31cef1615cfe75
Candra Davis <candrasd1029@gmail.com>
2016-05-02 14:50:47 -0500

    added basic index.html page

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
 var gulp     = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
 var markdown = require('gulp-markdown');
 var htmlmin  = require('gulp-htmlmin');
 var sequence  = require('gulp-sequence');
 var del      = require('del');
+var bSync    = require('browser-sync');
+var reload    = bSync.reload;
+
 
 gulp.task("blog", function() {
   return gulp.src("src/posts/**/*.md")
              .pipe(markdown())
              .pipe(htmlmin({collapseWhitespace: true}))
-             .pipe(gulp.dest("dist/blog"));
+             .pipe(gulp.dest("dist/blog"))
+             .pipe(reload({stream: true}));
 });
 
+gulp.task('pages', function() {
+  return gulp.src("src/pages/**/*.html")
+             .pipe(nunjucksRender({ path: ['src/templates']}))
+             .pipe(gulp.dest('dist'))
+             .pipe(reload({stream: true}));
+});
 
 gulp.task("clean", function() {
   del(['dist']);
 });
 
-gulp.task("default", sequence('clean', 'blog') );
+gulp.task("sync", function() {
+  return bSync({server: {baseDir: 'dist'}});
+});
+
+gulp.task("default", sequence('clean', 'blog','pages', 'sync') );
