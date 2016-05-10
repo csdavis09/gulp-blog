50684878ab8984f19709f057ca2f259e0bc0a9eb
Candra Davis <candrasd1029@gmail.com>
2016-05-09 23:56:28 -0500

    template changes and images and bio

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
+var site = {
+  title: "She can code-it"
+};
+
 var gulp     = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 var markdown = require('gulp-markdown');
+var frontMatter = require('gulp-front-matter');
 var htmlmin  = require('gulp-htmlmin');
 var sass     = require('gulp-sass');
 var sequence  = require('gulp-sequence');
 var concat  = require('gulp-concat');
+var data  = require('gulp-data');
 var del      = require('del');
+var through2      = require('through2');
 var bSync    = require('browser-sync');
 var reload    = bSync.reload;
 
 
+var collectPosts = function() {
+  var posts = [];
+
+  return through2.obj(function(file, enc, cb) {
+    var post = file.page;
+    post.content = file.contents;
+    posts.push(post);
+    this.push(file);
+    cb();
+  }, function(cb) {
+    site.posts = posts;
+    cb();
+  });
+}
+
 gulp.task("blog", function() {
   return gulp.src("src/posts/**/*.md")
+             .pipe(frontMatter({property: "page", remove: true}))
+             .pipe(data({site: site}))
              .pipe(markdown())
+             .pipe(collectPosts())
              .pipe(htmlmin({collapseWhitespace: true}))
              .pipe(gulp.dest("dist/blog"))
              .pipe(reload({stream: true}));
@@ -20,6 +45,7 @@ gulp.task("blog", function() {
 
 gulp.task('pages', function() {
   return gulp.src("src/pages/**/*.html")
+             .pipe(data({site: site}))
              .pipe(nunjucksRender({ path: ['src/templates']}))
              .pipe(htmlmin({collapseWhitespace: true}))
              .pipe(gulp.dest('dist'))
@@ -34,6 +60,11 @@ gulp.task('sass', function()  {
              .pipe(reload({stream: true}));
 });
 
+gulp.task("images", function() {
+  return gulp.src("src/img/**/*")
+             .pipe(gulp.dest("dist/img"));
+});
+
 gulp.task("clean", function() {
   del(['dist']);
 });
@@ -42,4 +73,4 @@ gulp.task("sync", function() {
   return bSync({server: {baseDir: 'dist'}});
 });
 
-gulp.task("default", sequence('clean', 'sass', 'blog', 'pages', 'sync') );
+gulp.task("default", sequence('clean', 'images', 'sass', 'blog', 'pages', 'sync') );
--- src/posts/second-post.md
+++ src/posts/second-post.md
@@ -1,5 +1,5 @@
 ---
-title: First post
+title: Second post
 author: Candra
 ---
 
