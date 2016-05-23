a3d2f59a3b0cba4cebe821e14bb558b05b6ef0a3
Candra Davis <candrasd1029@gmail.com>
2016-05-23 12:37:51 -0500

    -a

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,26 @@
   };
 
   var gulp     = require('gulp');
-  var nunjucksRender = require('gulp-nunjucks-render');
-  var markdown = require('gulp-markdown');
-  var frontMatter = require('gulp-front-matter');
-  var htmlmin  = require('gulp-htmlmin');
-  var sass     = require('gulp-sass');
-  var imagemin     = require('gulp-imagemin');
-  var sequence  = require('gulp-sequence');
-  var ghPages  = require('gulp-gh-pages');
-  var watch  = require('gulp-watch');
-  var concat  = require('gulp-concat');
-  var data  = require('gulp-data');
+  var plugins  = require('gulp-load-plugins')();
   var del      = require('del');
-  var through2      = require('through2');
+  var fs      = require('fs');
+  var through2 = require('through2');
   var bSync    = require('browser-sync');
-  var reload    = bSync.reload;
+  var reload   = bSync.reload;
 
+
+  var summarize = function(string, marker) {
+    return string.split(marker)[0];
+  };
 
   var collectPosts = function() {
     var posts = [];
 
     return through2.obj(function(file, enc, cb) {
       var post = file.page;
+      post.body = file.contents.toString();
+      post.summary = summarize(file.contents.toString(), '<!--more-->');
       post.content = file.contents;
-console.log(file.page.permalink);
       posts.push(post);
       this.push(file);
       cb();
@@ -41,49 +37,51 @@ console.log(file.page.permalink);
 
   gulp.task("blog", function() {
     return gulp.src("src/posts/**/*.md")
-	      .pipe(frontMatter({property: "page", remove: true}))
-	      .pipe(data({site: site}))
-	      .pipe(markdown())
+	      .pipe(plugins.frontMatter({property: "page", remove: true}))
+	      .pipe(plugins.data({site: site}))
+	      .pipe(plugins.markdown())
 	      .pipe(collectPosts())
-	      .pipe(htmlmin({collapseWhitespace: true}))
+              .pipe(plugins.wrap(function (data) {
+                return fs.readFileSync('src/templates/post.html').toString();
+              }, null, {engine: 'nunjucks'}))
+	      .pipe(plugins.htmlmin({collapseWhitespace: true}))
 	      .pipe(gulp.dest("dist/blog"))
 	      .pipe(reload({stream: true}));
   });
 
   gulp.task('pages', function() {
     return gulp.src("src/pages/**/*.html")
-	      .pipe(data({site: site}))
-	      .pipe(nunjucksRender({ path: ['src/templates']}))
-	      .pipe(htmlmin({collapseWhitespace: true}))
+	      .pipe(plugins.data({site: site}))
+	      .pipe(plugins.nunjucksRender({ path: ['src/templates']}))
+	      .pipe(plugins.htmlmin({collapseWhitespace: true}))
 	      .pipe(gulp.dest('dist'))
 	      .pipe(reload({stream: true}));
   });
 
   gulp.task('sass', function()  {
     return gulp.src("src/css/**/*.scss")
-	      .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
-	      .pipe(concat('css/all.min.css', {newLine: ''}))
+	      .pipe(plugins.sass({outputStyle: 'compact'}).on('error', plugins.sass.logError))
+	      .pipe(plugins.concat('css/all.min.css', {newLine: ''}))
 	      .pipe(gulp.dest('dist'))
 	      .pipe(reload({stream: true}));
   });
 
   gulp.task("images", function() {
     return gulp.src("src/img/**/*")
-	      .pipe(imagemin())
-	      .pipe(gulp.dest("dist/img"));
+	       .pipe(plugins.imagemin())
+	       .pipe(gulp.dest("dist/img"));
   });
-
 
   gulp.task("js", function() {
     return gulp.src("src/js/**/*.js")
-	      .pipe(gulp.dest("dist/js"));
+	       .pipe(gulp.dest("dist/js"));
   });
 
   gulp.task('deploy', function() {
-    return gulp.src('src/js/**/*')
-               .pipe(ghPages());                 
- });               
-  
+    return gulp.src('dist/**/*')
+               .pipe(plugins.ghPages());
+ });
+
   gulp.task('watch', function(cb) {
     gulp.watch('src/js/**/*.js', ['js']);
     gulp.watch('src/css/**/*.scss', ['sass']);
@@ -92,7 +90,6 @@ console.log(file.page.permalink);
     gulp.watch('src/img/**/*', ['images']);
     cb();
   });
-
 
   gulp.task('assets', ['images', 'sass', 'js']);
 
@@ -104,5 +101,6 @@ console.log(file.page.permalink);
     return bSync({server: {baseDir: 'dist'}});
   });
 
-  gulp.task("default", sequence('clean', 'assets', 'blog', 'pages', 'watch', 'sync') );
+  gulp.task("default", plugins.sequence('clean', 'assets', 'blog', 'pages', 'watch', 'sync') );
+
 }());
---
+++ src/posts/choosing-the-right-school.md
@@ -0,0 +1,23 @@
+---
+title: Choosing the Right School
+author: Candra
+permalink: /blog/choosing-the-right-school.html
+---
+
+When looking to find the right school, it's important to consider
+several different factors.  What is important to you?  What are your learning styles?
+How will you best balance school with home?
+
+There are a plethora of online and in-person bootcamp options. 
+However, choosing the right school can be daunting.  One of the things
+that was most frustrating for me was finding accurate representations
+of what the school was like. Inevetiably the reviews available were of
+only the students that successfully completed the program.  Yet, when I 
+would ask recruiting officers what percentage of their admitted students
+completed the program, sometimes the numbers of drop-outs were astounding.
+
+Yet, these were supposed to be cream of the crop, the best and most reputable
+programs.  One such program had a drop out rate of 56% for the previous
+year of students.  When you are thinking about spending several thousands
+of dollars for a program, which inevitably has a partial refund policy,
+it would be......blah, blah, blah.
